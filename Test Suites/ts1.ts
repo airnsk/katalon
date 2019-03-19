c51a81d61da4bc8dfeaa2545d7533d0a666b6240
alex <alex@192.168.1.6>
2019-03-20 02:22:21 +0300

    add ts1

---
+++ Test Suites/ts1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5fadadb2-0d13-4cb1-b0b0-c6080a56de89</testSuiteGuid>
+   <testCaseLink>
+      <guid>f1e19726-2cbd-4d38-a412-242646629524</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case (1)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
