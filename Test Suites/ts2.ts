b933fcb3d106f676a71bf28d203c6ee005377ab5
alex <alex@192.168.1.6>
2019-03-20 02:29:38 +0300

    add ts2

---
+++ Test Suites/ts2.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>88206cc0-1d67-492d-b8b6-aa0241096e32</testSuiteGuid>
+   <testCaseLink>
+      <guid>0dde4792-ae21-493a-ab66-d6c1f37c7212</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a7896eff-aec8-4809-841c-4f52cc7eb237</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case (1)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
